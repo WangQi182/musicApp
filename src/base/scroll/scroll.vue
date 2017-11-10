24a8691e23e835c1825dac2c4fa5677be3eb563e
wangqi <577089091@qq.com>
2017-11-10 18:22:45 +0800

    ok

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,77 @@
+<template>
+  <div ref='warpper'>
+  	<slot></slot>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  export default {
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      data: {
+        type: Array,
+        default: null
+      },
+      listenScroll: {
+        type: Boolean,
+        default: false
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._initScroll()
+      }, 20)
+    },
+    methods: {
+      _initScroll() {
+        if (!this.$refs.warpper) {
+          return
+        }
+        this.scroll = new BScroll(this.$refs.warpper, {
+          probeType: this.probeType,
+          click: this.click
+        })
+        if (this.listenScroll) {
+          let me = this
+          this.scroll.on('scroll', (pos) => {
+            console.log(pos)
+            me.$emit('scroll', pos)
+          })
+        }
+      },
+      enable() {
+        this.scroll && this.scroll.enable()
+      },
+      disable() {
+        this.scroll && this.scroll.disable()
+      },
+      refresh() {
+        console.log(111)
+        this.scroll && this.scroll.refresh()
+      },
+      scrollTo() {
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      },
+      scrollToElement() {
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+      }
+    },
+    watch: {
+      data() {
+        setTimeout(() => {
+          console.log(1)
+          this.refresh()
+        }, 20)
+      }
+    }
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
